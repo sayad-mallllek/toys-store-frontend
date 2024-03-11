d6f8c7625dbdb6496556fadbf7b28543ad42c188
sayad-mallllek <bob@khalilelzein.com>
2024-03-12 00:17:36 +0200

    added hover on links

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -17,11 +17,11 @@ const links = [
 
 const Header = () => {
   return (
-    <header className="py-10 px-5 flex gap-4 items-center">
+    <header className="py-10 px-5 flex gap-4 sm:gap-6 md:gap-8 items-center">
       <img src="/images/logo.png" alt="Logo" className="w-20 h-20" />
       {links.map(({ href, label }) => (
         <Link
-          className="text-gray-500 font-semibold"
+          className="text-gray-500 font-semibold hover:text-gray-900 transition-colors duration-200"
           key={`${href}${label}`}
           href={href}
         >
