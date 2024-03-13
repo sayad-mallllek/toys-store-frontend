578cf3b72707ee0857a57655a4dee325f6e9d9b0
sayad-mallllek <bob@khalilelzein.com>
2024-03-14 00:48:02 +0200

    more stuff to add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,15 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main>
-      <div className="flex justify-center gap-16"></div>
+      <div className="flex justify-center gap-16">
+        <div>hello</div>
+        <Image
+          src={"/images/home-header-dotted.png"}
+          alt="home header"
+          width={400}
+          height={400}
+        />
+      </div>
     </main>
   );
 }
